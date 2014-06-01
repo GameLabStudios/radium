0ff501922ccabecd4aa0f5eb7264582b0c0988b8
anchgu7 <soccerplayer7ag@msn.com>
2014-06-01 16:49:49 -0700

    Comment out print statements in ChasePlayer.

--- Radium/ChasePlayer.cpp
+++ Radium/ChasePlayer.cpp
@@ -14,26 +14,26 @@ ChasePlayer::~ChasePlayer()
 
 BNodeStatus Action::run()
 {
-    if (mPlayer->isShooting)
-    {
-        nodeStatus = RUNNING;
-    }
-    else
-    {
-        nodeStatus = SUCCESS;
-    }
+    //if (mPlayer->isShooting)
+    //{
+    //    nodeStatus = RUNNING;
+    //}
+    //else
+    //{
+    //    nodeStatus = SUCCESS;
+    //}
     bTree->currentNode = this;
     return nodeStatus;
 }
 
 void Action::doAction()
 {
-    if (nodeStatus == SUCCESS)
-    {
-        cout << "caught player" << endl;
-    }
-    else if (nodeStatus == RUNNING)
-    {
-        cout << "still chasing player" << endl;
-    }
+    //if (nodeStatus == SUCCESS)
+    //{
+    //    cout << "caught player" << endl;
+    //}
+    //else if (nodeStatus == RUNNING)
+    //{
+    //    cout << "still chasing player" << endl;
+    //}
 }
